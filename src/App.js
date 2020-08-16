84635bddcb8fbbdd2ba8bd57873e0fc0962ec644
Leanne Chen <lianchennnn@gmail.com>
2020-08-16 23:23:05 +0800

    Styling welcome page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Fragment } from 'react';
-import logo from './logo.svg';
 import { makeStyles } from '@material-ui/core/styles';
-import { Container, CssBaseline, Typography, Paper } from '@material-ui/core';
+import { Container, CssBaseline, Typography, Paper, Box, Button, Grid } from '@material-ui/core';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   root: {
     // maxWidth: `600px`,
   },
-});
+  paper: {
+  },
+  welcomeText: {
+    marginBottom: theme.spacing(2),
+  }
+}));
 
 function App() {
   const classes = useStyles();
@@ -16,51 +20,24 @@ function App() {
     <Fragment>
       <CssBaseline/>
       <Container maxWidth="md">
-        <Paper className={classes.root} my={5}>
-          <Typography variant="h1" component="h2" gutterBottom>
-            h1. Heading
+        <Box marginTop={5}>
+          <Typography variant="h4" component="h2" gutterBottom>
+            My Profile
           </Typography>
-          <Typography variant="h2" gutterBottom>
-            h2. Heading
-          </Typography>
-          <Typography variant="h3" gutterBottom>
-            h3. Heading
-          </Typography>
-          <Typography variant="h4" gutterBottom>
-            h4. Heading
-          </Typography>
-          <Typography variant="h5" gutterBottom>
-            h5. Heading
-          </Typography>
-          <Typography variant="h6" gutterBottom>
-            h6. Heading
-          </Typography>
-          <Typography variant="subtitle1" gutterBottom>
-            subtitle1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
-          </Typography>
-          <Typography variant="subtitle2" gutterBottom>
-            subtitle2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
-          </Typography>
-          <Typography variant="body1" gutterBottom>
-            body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
-            unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam
-            dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
-          </Typography>
-          <Typography variant="body2" gutterBottom>
-            body2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
-            unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam
-            dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
-          </Typography>
-          <Typography variant="button" display="block" gutterBottom>
-            button text
-          </Typography>
-          <Typography variant="caption" display="block" gutterBottom>
-            caption text
-          </Typography>
-          <Typography variant="overline" display="block" gutterBottom>
-            overline text
-          </Typography>
-        </Paper>
+        </Box>
+        <Box paddingY={5} paddingX={4} clone>
+          <Paper className={classes.paper}>
+            {/* welcome */}
+            <Grid align="center">
+              <Typography variant="body1" gutterBottom align="center" className={classes.welcomeText}>
+                You haven't input your profile yet.
+              </Typography>
+              <Button variant="contained" color="primary" size="large">
+                Let's create one!
+              </Button>
+            </Grid>
+          </Paper>
+        </Box>
       </Container>
     </Fragment>
   )
