35768e2d1f94c9553c066a7212e377a80ad45117
Leanne Chen <lianchennnn@gmail.com>
2020-08-16 21:43:12 +0800

    init

--- src/App.js
+++ src/App.js
@@ -2,11 +2,29 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+    maxWidth: 500,
+  },
+});
+
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
+    <div className={`${classes.root} App`}>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Typography variant="h1" component="h2" gutterBottom>
+          h1. Heading
+        </Typography>
+        <Typography variant="h2" gutterBottom>
+          h2. Heading
+        </Typography>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
